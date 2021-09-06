589d3b5441cd215c2feaab6cd675851292dc5d25
Antonio Silva <antonio.ffsmail@gmail.com>
2021-09-06 11:07:36 -0300

    final primeiro curso, appreceita conectado ao banco

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Receita
+
+
+class ListReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
+    list_display_links = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
+
 
 # Register your models here.
+
+admin.site.register(Receita, ListReceitas)
+
