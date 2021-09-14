9def7ef160488e450ea751c520bec35fdb40c75d
Antonio Silva <antonio.ffsmail@gmail.com>
2021-09-14 09:07:08 -0300

    refatorando views e urls

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from receitas.models import Receita
@@ -71,31 +71,13 @@ def dashboard(request):
 def logout(request):
     auth.logout(request)
     return redirect('index')
-    
-def cria_receita(request):
-    if(request.method == 'POST'):
-        nome_receita = request.POST['nome_receita']
-        ingredientes = request.POST['ingredientes']
-        modo_preparo = request.POST['modo_preparo']
-        tempo_preparo = request.POST['tempo_preparo']
-        rendimento = request.POST['rendimento']
-        categoria = request.POST['categoria']
-        foto_receita = request.FILES['foto_receita']
-        user = get_object_or_404(User, pk=request.user.id)
-
-        receita = Receita.objects.create(pessoa=user,nome_receita=nome_receita, ingredientes=ingredientes, modo_preparo=modo_preparo, 
-        tempo_preparo=tempo_preparo, rendimento=rendimento, categoria=categoria, foto_receita=foto_receita)
-
-        receita.save()
-
-        return redirect('dashboard')
-
-    else:
-        messages.error(request, "O preenchimento de todos os campos são obrigatórios")
-        return render(request, 'usuarios/cria_receita.html')
 
 def valida_campo_vazio(campo):
     return not campo.strip()
 
 def senhas_nao_sao_iguais(password1, password2):
     return password1 != password2
+
+
+
+
